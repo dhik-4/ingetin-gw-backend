2e2f2dc7fc3abebd0985d9a971c4623e5afc2584
LAPTOP-2HP7GB51\Lenovo <andhikaekananda@gmail.com>
2025-12-29 09:27:50 +0700

    add new for access token table in database

---
+++ Models/AccessToken.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IngetinGwAPI.Models;
+
+public partial class AccessToken
+{
+    public int Id { get; set; }
+
+    public int UsersId { get; set; }
+
+    public string Token { get; set; } = null!;
+
+    public DateTime CreatedAt { get; set; }
+
+    public DateTime? ExpiresAt { get; set; }
+
+    public byte? IsRevoked { get; set; }
+}
